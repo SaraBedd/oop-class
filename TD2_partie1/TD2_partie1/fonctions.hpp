8e5665d9dc0ec00d0889f8daf6945e661ae9bd38
SaraB <sara.beddouch@polymtl.ca>
2021-05-20 18:50:39 -0400

    Part 1 Done for real this time
    No leak wooohooo

--- TD2_partie1/TD2_partie1/fonctions.hpp
+++ TD2_partie1/TD2_partie1/fonctions.hpp
@@ -43,15 +43,14 @@ gsl::span<Designer*> spanListeDesigners(const ListeDesigners& ld)
 //TODO: Fonction qui cherche un designer par son nom dans une ListeJeux
 // Cette fonction renvoie le pointeur vers le designer si elle le trouve dans
 // un des jeux de la ListeJeux. En cas contraire, elle renvoie un pointeur nul.
-Designer* rechercherDesignerNom(const ListeJeux& lj, string nom) {
+Designer* rechercherDesigner(const ListeJeux& lj, string nom) {
 	for (Jeu* j : spanListeJeux(lj)) {
 		for (Designer* d : spanListeDesigners(j->designers)) {
-			if (d->nom == nom) {
+			if (d->nom.compare(nom) == 0) {
 				return d;
 			}
 		}
 	}
-	cout << "Le designer recherche n'existe pas." << "\n";
 	return nullptr;
 }
 
@@ -69,14 +68,15 @@ Designer* lireDesigner(std::istream& f, const ListeJeux& lj)
 	// on va avoir des doublons car plusieurs jeux ont des designers en commun
 	// dans le fichier binaire. Afficher un message lorsque l'allocation du
 	// designer est réussie
-	Designer* designerLu = new Designer;
-	designerLu = rechercherDesignerNom(lj, d.nom);
-
-	if (designerLu == nullptr) {
-		*designerLu = d;
+	if (rechercherDesigner(lj, d.nom) == nullptr) {
+		Designer* designerLu = new Designer(d);
+		cout << "Allocation en memoire du designer " << "\033[1;32m" << designerLu->nom << "\033[0m" << " est reussie" << "\n";
+		return designerLu;
 	}
+	else {
+		return rechercherDesigner(lj, d.nom);
 
-	return designerLu;
+	}
 }
 
 //TODO: Fonction qui double le tableau de jeux de ListeJeux
@@ -85,42 +85,27 @@ Designer* lireDesigner(std::istream& f, const ListeJeux& lj)
 // avait dans l'ancien, et éliminer l'ancien trop petit. N'oubliez pas, on copie
 // des pointeurs de jeux. Il n'y a donc aucune nouvelle allocation ici !
 void doublerTaille(unsigned int nouvelleTaille, ListeJeux& lj) {
-
-	Jeu** TableauJeux = new Jeu * [nouvelleTaille];
-	int index = 0;
-	for (Jeu* j : spanListeJeux(lj)) {
-		if (j != nullptr) {
-			TableauJeux[index] = j;
-			index++;
-		}
+	Jeu** tableauDouble = new Jeu * [nouvelleTaille] {};
+	for (int n : iter::range(lj.nElements)) {
+		tableauDouble[n] = lj.elements[n];
 	}
 	delete[] lj.elements;
 	lj.capacite = nouvelleTaille;
-	lj.elements = TableauJeux;
-	return;                                            // Est-ce que l'on retourne quelque chose?
+	lj.elements = tableauDouble;
 }
-
 
 //TODO: Fonction pour ajouter un Jeu à ListeJeux
 // Le jeu existant déjà en mémoire, on veut uniquement ajouter le pointeur vers
 // le jeu existant. De plus, en cas de saturation du tableau elements, cette
 // fonction doit doubler la taille du tableau elements de ListeJeux et détruire
 // l'ancien. Utilisez la fonction de doublement écrite plus haut.
-Jeu* ajouterJeu(Jeu jeu, ListeJeux& lj) {
-	
-	if (lj.capacite == 0) {
-		// TODO warning pour la capacite = 0
-		cout << "capacite est de 0, mais elle doit etre d'au moins 1" << "\n";
-		return nullptr;
-	}
+void ajouterJeu(Jeu* j, ListeJeux& lj) {
 	if (lj.nElements == lj.capacite) {
-		doublerTaille(lj.capacite * 2, lj);
+		doublerTaille(max((unsigned int)1, lj.capacite * 2), lj);
 	}
-	lj.nElements += 1;
-	*(lj.elements[lj.nElements]) = jeu;
-	return lj.elements[lj.nElements];
+	lj.elements[lj.nElements] = j;
+	lj.nElements++;
 }
-
 
 Jeu* lireJeu(std::istream& f, ListeJeux& lj)
 {
@@ -137,29 +122,14 @@ Jeu* lireJeu(std::istream& f, ListeJeux& lj)
 	// que contient un jeu. Servez-vous de votre fonction d'ajout de jeu car la
 	// liste de jeux participé est une ListeJeu. Afficher un message lorsque
 	// l'allocation du jeu est réussie
-	Jeu* jeuLu = new Jeu;
-	jeuLu = ajouterJeu(j, lj);
-	
-	// Ici on considère comme taille la capacité du tableau de designers à la position 0 de lj.elements, donc le premier jeu sauvegarder dans la listeJeu
-	Designer** tableauDesigners = new Designer*[j.designers.nElements];
-	
-	ListeJeux listeJeuxParticipes = {};
-	listeJeuxParticipes.capacite = 1;
-	listeJeuxParticipes.nElements = 0;
-
-	for (int n : iter::range(j.designers.nElements))
-	{
-		lj.nElements = n;
-		//TODO: Ajouter un jeu à ListeJeux
-		tableauDesigners[n] = lireDesigner(f, lj);																					// On fait quoi avec la capacite?
-		tableauDesigners[n]->listeJeuxParticipes.elements[listeJeuxParticipes.nElements] = ajouterJeu(j, listeJeuxParticipes);
-		listeJeuxParticipes.nElements++;
-		cout << "Allocation en memoire du designer " << tableauDesigners[n]->nom << " est reussie" << "\n";
+	Jeu* jeuLu = new Jeu(j);
+	cout << "\n" << "\033[1;32mAllocation en memoire du jeu " << jeuLu->titre << " est reussie\033[0m\n";
+	// cout << "Allocation en memoire du jeu " << jeuLu->titre << " est reussie" << "\n";
+	jeuLu->designers.elements = new Designer * [j.designers.nElements]{};
+	for (int n : iter::range(jeuLu->designers.nElements)) {
+		jeuLu->designers.elements[n] = lireDesigner(f, lj);
+		ajouterJeu(jeuLu, jeuLu->designers.elements[n]->listeJeuxParticipes);
 	}
-
-	// Ici on affecte a Jeu* jeuLu le tableau de designer
-	jeuLu->designers.elements = tableauDesigners;
-	cout << "Allocation en memoire du jeu " << jeuLu->titre << " est reussie" << "\n";
 	return jeuLu;
 }
 
@@ -169,68 +139,29 @@ ListeJeux creerListeJeux(const std::string nomFichier)
 	f.exceptions(std::ios::failbit);
 	int nElements = lireUint16(f);
 	ListeJeux lj = {};
-	lj.capacite = nElements;
-	for(int n : iter::range(nElements))
+
+	for (int n : iter::range(nElements))
 	{
 		lj.nElements = n;
 		//TODO: Ajouter un jeu à ListeJeux
-		lj.elements[n] = lireJeu(f, lj);																								// On fait quoi avec la capacite?
+		ajouterJeu(lireJeu(f, lj), lj);
 	}
 	lj.nElements = nElements;
 	//TODO: Renvoyez la ListeJeux
 	return lj;
 }
 
-//TODO: Fonction qui détermine si un designer participe encore à un jeu
-bool designerParticipe(const ListeJeux& lj, string nomDesigner) {
-	Designer* designer = new Designer;
-	designer = rechercherDesignerNom(lj, nomDesigner);
+//TODO: Fonction qui détermine si un designer participe encore à un jeu OK
+bool designerParticipe(Designer* designer) {
 	return designer->listeJeuxParticipes.nElements > 0;
-}
-
-void swap(int*& a, int*& b)
-{
-	int* c = a;
-	a = b;
-	b = c;
 }
 
 //TODO: Fonction pour détruire un designer (libération de mémoire allouée)
 // Lorsqu'on détruit un designer, on affiche son nom pour fins de débogage
-
-/*void detruireDesigner(ListeJeux& lj, string nomDesigner) {
-	Designer* designer = new Designer;
-	Designer* designerTemp = new Designer;
-	for (Jeu* j : spanListeJeux(lj)) {
-		designer = rechercherDesignerNom(lj, nomDesigner);
-		if (designer != nullptr) {
-			delete[] designer->listeJeuxParticipes.elements;			// Est-ce que delete designer deleterait listeJeuxPartivipe?
-			delete designer;											
-				cout << "Destruction du designer " << nomDesigner << " est reussie pour le jeu " << j->titre << "\n";
-		}
-		else {
-			cout << "Designer " << nomDesigner << " n'existe pas pour le jeu " << j->titre << "\n";
-		}
-	}
-}*/
-
-void detruireDesigner(ListeJeux& lj, string nomDesigner) {
-	Designer* designer = new Designer;
-	Designer* designerTemp = new Designer;
-	for (Jeu* j : spanListeJeux(lj)) {
-		designer = rechercherDesignerNom(lj, nomDesigner);
-		if (designer != nullptr) {
-			designerTemp = j->designers.elements[j->designers.nElements];
-			swap(designer, designerTemp);
-			delete[] designer->listeJeuxParticipes.elements;			// Est-ce que delete designer deleterait listeJeuxPartivipe?
-			delete designer;
-			j->designers.nElements--;
-			cout << "Destruction du designer " << nomDesigner << " est reussie pour le jeu " << j->titre << "\n";
-		}
-		else {
-			cout << "Designer " << nomDesigner << " n'existe pas pour le jeu " << j->titre << "\n";
-		}
-	}
+void detruireDesigner(Designer* designer) {
+	cout << "Le designer  " << "\033[1;31m" << designer->nom << "\033[0m" << " est supprime avec succes" << "\n";
+	delete[] designer->listeJeuxParticipes.elements;
+	delete designer;
 }
 
 //TODO: Fonction qui enlève un jeu de ListeJeux
@@ -238,18 +169,16 @@ void detruireDesigner(ListeJeux& lj, string nomDesigner) {
 // pointeur de la ListeJeux, mais le jeu pointé existe encore en mémoire.
 // Puisque l'ordre de la ListeJeux n'a pas être conservé, on peut remplacer le
 // jeu à être retiré par celui présent en fin de liste et décrémenter la taille
-// de celle-ci
-Jeu* enleverJeu(ListeJeux& lj, string titre) {
-	Jeu* jeuTemp = new Jeu;
-	jeuTemp = lj.elements[lj.nElements];
-	for (Jeu* j : spanListeJeux(lj)) {
-		if (j->titre == titre) {
-			swap(j, jeuTemp);
+// de celle-ci 
+void enleverJeu(ListeJeux& lj, Jeu* jeu) {
+	for (Jeu*& j : spanListeJeux(lj)) {
+		if (j == jeu) {
+			if (lj.nElements > 1) {
+				j = lj.elements[lj.nElements - 1];
+			}
 			lj.nElements--;
-			return j;
 		}
 	}
-	return nullptr;
 }
 
 //TODO: Fonction pour détruire un jeu (libération de mémoire allouée)
@@ -258,53 +187,34 @@ Jeu* enleverJeu(ListeJeux& lj, string titre) {
 // une ListeDesigners). On doit commencer par enlever le jeu à détruire des jeux
 // qu'un designer a participé (listeJeuxParticipes). Si le designer n'a plus de
 // jeux présents dans sa liste de jeux participés, il faut le supprimer.  Pour
-// fins de débogage, affichez le nom du jeu lors de sa destruction.
-void detruireJeu(ListeJeux& lj, string titre) {
-	Jeu* jeuDetruit = new Jeu;
-	Designer* designerTemp = new Designer;
-
-	for (Jeu* j : spanListeJeux(lj)) {
-		for (Designer* d : spanListeDesigners(j->designers)) {
-			jeuDetruit = enleverJeu(d->listeJeuxParticipes, titre);
-			if (jeuDetruit != nullptr) {
-				delete jeuDetruit;
-				cout << "le jeu " << titre << " est detruit de la liste jeux participe de " << d->nom << "\n";
-			}
-			if (d->listeJeuxParticipes.nElements == 0) {
-				designerTemp = j->designers.elements[j->designers.nElements];
-				swap(d, designerTemp);
-				string nomDesigner = d->nom;
-				delete[] d->listeJeuxParticipes.elements;			// Est-ce que delete designer deleterait listeJeuxPartivipe?
-				delete d;
-				j->designers.nElements--;
-				cout << "Destruction du designer " << nomDesigner << " est reussie pour le jeu " << j->titre << "\n";
-			}
+// fins de débogage, affichez le nom du jeu lors de sa destruction. OK
+void detruireJeu(ListeJeux& lj, Jeu* jeu) {
+	for (Designer* d : spanListeDesigners(jeu->designers)) {
+		enleverJeu(d->listeJeuxParticipes, jeu);
+		if (!designerParticipe(d)) {
+			detruireDesigner(d);
 		}
-		jeuDetruit = enleverJeu(lj, titre);
-		delete[] jeuDetruit->designers.elements;
-		delete jeuDetruit;
-		cout << "le jeu " << titre << " est detruit." << "\n";
 	}
+	cout << "\033[1;31mLe jeu " << jeu->titre << " est supprime avec succes\033[0m\n\n";
+	// cout << "Le jeu " << jeu->titre << " est supprime avec succes" << "\n";
+	delete[] jeu->designers.elements;
+	delete jeu;
 }
 
-
-//TODO: Fonction pour détruire une ListeJeux et tous ses jeux
+//TODO: Fonction pour détruire une ListeJeux et tous ses jeux OK
 void detruireListeJeu(ListeJeux& lj) {
 	for (Jeu* j : spanListeJeux(lj)) {
-		detruireJeu(lj, j->titre);
+		detruireJeu(lj, j);
 	}
-	lj.capacite = 0;
-	lj.nElements = 0;
-	delete &lj;
+	delete[] lj.elements;
 }
-
-
 
 void afficherDesigner(const Designer& d)
 {
 	std::cout << "\t" << d.nom << ", " << d.anneeNaissance << ", " << d.pays
-			  << std::endl;
+		<< std::endl;
 }
+
 //TODO: Fonction pour afficher les infos d'un jeu ainsi que ses designers
 // Servez-vous de la fonction afficherDesigner ci-dessus
 void afficherJeu(const Jeu& j) {
@@ -322,10 +232,9 @@ void afficherJeu(const Jeu& j) {
 // de séparation doit être différent de celle utilisée dans le main
 // /utf-8 
 void afficherListeJeu(const ListeJeux& lj) {
-	cout << "Les jeux de la liste sont: " << "\n";
 	for (int n : iter::range(lj.nElements)) {
-		cout << "*********************************" << "\n";
+		cout << "-----------------------------------------------------" << "\n";
 		afficherJeu(*(lj.elements[n]));
-		cout << "*********************************" << "\n";
+		cout << "-----------------------------------------------------" << "\n";
 	}
 }
